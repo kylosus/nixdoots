2d67697e4e209c6af1cc7e5fa26ebad3943f7ff3
jokie <jokie@localhost>
2024-04-14 20:14:12 +0300

    Added gtk theme

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -99,6 +99,26 @@
     musicDirectory = "${config.xdg.userDirs.music}";
   };
 
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Catppuccin-Macchiato-Compact-Pink-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "pink" ];
+        size = "compact";
+        tweaks = [ "rimless" "black" ];
+        variant = "macchiato";
+      };
+    };
+  };
+
+# Now symlink the `~/.config/gtk-4.0/` folder declaratively:
+xdg.configFile = {
+  "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+  "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+  "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+};
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
