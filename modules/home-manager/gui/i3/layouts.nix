ab51d01a179eb1ea2bfd6a645e123fd7bbaba7b1
kylosus <jokersus.cava@gmail.com>
2024-04-21 16:27:10 +0300

    Moved i3-layouts to i3.config.startup instead of a systemd service
    This works better for in-place reload of i3

--- modules/home-manager/gui/i3/layouts.nix
+++ modules/home-manager/gui/i3/layouts.nix
@@ -19,19 +19,26 @@
     set $i3l autosplit to workspace 10
   '';
 
-  systemd.user.services = {
-    i3-layouts = {
-      Unit = {
-        Description = "i3 layouts";
-        PartOf = ["i3-session.target"];
-        # After = ["xrandr.service" "picom.service"];
-      };
-      Service = {
-        ExecStart = "${lib.getBin pkgs.i3-layouts}/bin/i3-layouts";
-        RemainAfterExit = true;
-        Type = "simple";
-      };
-      Install.WantedBy = ["i3-session.target"];
-    };
-  };
+  xsession.windowManager.i3.config.startup = [
+    {
+      command = "${lib.getBin pkgs.i3-layouts}/bin/i3-layouts";
+      always = true;
+    }
+  ];
+
+  # systemd.user.services = {
+  #   i3-layouts = {
+  #     Unit = {
+  #       Description = "i3 layouts";
+  #       PartOf = ["i3-session.target"];
+  #       # After = ["xrandr.service" "picom.service"];
+  #     };
+  #     Service = {
+  #       ExecStart = "${lib.getBin pkgs.i3-layouts}/bin/i3-layouts";
+  #       # RemainAfterExit = true;
+  #       Type = "simple";
+  #     };
+  #     Install.WantedBy = ["i3-session.target"];
+  #   };
+  # };
 }
