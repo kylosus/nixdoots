265b24e31212f93852db434cda3a6df9308abbce
kylosus <jokersus.cava@gmail.com>
2024-04-28 21:10:23 +0300

    Added a patch for enabling GPU on opium
    https://github.com/armbian/build/blob/main/patch/u-boot/u-boot-sunxi/allwinner-h616-GPU-enable-hack.patch

--- hosts/opium/pkgs/uboot/default.nix
+++ hosts/opium/pkgs/uboot/default.nix
@@ -20,5 +20,8 @@
   # Thanks, https://github.com/ryan4yin/nixos-rk3588/blob/main/pkgs/u-boot-radxa/build-from-source.nix
   patches = [
     ./version-hardcode.patch
+    # Adapted from
+    # https://github.com/armbian/build/blob/main/patch/u-boot/u-boot-sunxi/allwinner-h616-GPU-enable-hack.patch
+    ./enable-gpu.patch
   ];
 })
