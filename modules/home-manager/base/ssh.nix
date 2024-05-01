c0544053831489646b0dceba58a0386da2f95893
kylosus <jokersus.cava@gmail.com>
2024-05-01 21:18:40 +0300

    Added control master to ssh

--- modules/home-manager/base/ssh.nix
+++ modules/home-manager/base/ssh.nix
@@ -5,6 +5,8 @@
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
+    controlMaster = "yes";
+    controlPersist = "10m";
     extraConfig = ''
       # include ${config.sops.secrets.ssh-hosts.path}
       include ${config.home.homeDirectory}/.ssh/hosts.d/hosts
