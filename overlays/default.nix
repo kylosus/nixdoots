c415d6e79d0e7168350ea7e4b812e15173679a1a
kylosus <jokersus.cava@gmail.com>
2024-05-05 22:15:38 +0300

    Removed nebula-master, did some cleanup

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,42 +7,6 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
-
-    #asusctl = prev.asusctl.override (old: {
-    #  rustPlatform =
-    #    old.rustPlatform
-    #    // {
-    #      buildRustPackage = args:
-    #       old.rustPlatform.buildRustPackage (args
-    #          // {
-    #            src = final.fetchFromGitLab {
-    #              owner = "asus-linux";
-    #              repo = "asusctl";
-    #              rev = "5.0.10";
-    #              hash = "sha256-H8x3nfOFRv9DkbDkFw+LO1tdHiVyU3SzetqED4twPSk=";
-    #            };
-    #            # cargoHash = "sha256-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=";
-    #            # cargoHash = "sha256-753560b6ae8edf72a4a9c6f0e5b187590184b1106375634ac2a95ad245c47275";
-    #            cargoHash = "sha256-H8x3nfOFRv9DkbDkFw+LO1tdHiVyU3SzetqED4twPSk=";
-    #            cargoLock = {
-    #             lockFile = ./Cargo.lock;
-    #              outputHashes = {
-    #                "ecolor-0.21.0" = "sha256-m7eHX6flwO21umtx3dnIuVUnNsEs3ZCyOk5Vvp/lVfI=";
-    #                "notify-rust-4.6.0" = "sha256-jhCgisA9f6AI9e9JQUYRtEt47gQnDv5WsdRKFoKvHJs=";
-    #                "supergfxctl-5.1.2" = "sha256-WDbUgvWExk5cs2cpjo88CiROdEbc01o2DELhRi9gju4=";
-    #              };
-    #            };
-    #          });
-    #    };
-    #});
-
-    # rxvt-unicode = prev.rxvt-unicode.overrideAttrs (old: {
-    #   emojiSupport = true;
-    # });
-
     pywal = prev.pywal.overrideAttrs (old: {
       src = prev.fetchFromGitHub {
         owner = "dylanaraps";
@@ -56,12 +20,6 @@
 
       propagatedBuildInputs = old.propagatedBuildInputs ++ [final.imagemagick];
     });
-
-    #    pywal = inputs.nixpkgs.legacyPackages.x86_64-linux.pkgs.python3.override {
-    #      packageOverrides = self: super: {
-    #
-    #      };
-    #    };
 
     ranger =
       (prev.ranger.overrideAttrs (old: {
@@ -81,19 +39,6 @@
     networkmanager = prev.networkmanager.override {
       openconnect = null;
     };
-
-    endlessh-go = prev.endlessh-go.overrideAttrs (oldAttrs: {
-      nativeBuildInputs = oldAttrs.nativeBuildInputs or [] ++ [final.makeWrapper];
-
-      postInstall =
-        oldAttrs.postInstall
-        or ""
-        + ''
-          wrapProgram $out/bin/endlessh-go --add-flags -geoip_supplier --add-flags ip-api
-        '';
-    });
-
-    nebula = prev.nebula-master;
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
