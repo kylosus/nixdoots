187b890d46f7e6e210ee524096b811052f003a2b
kylosus <jokersus.cava@gmail.com>
2024-08-26 20:21:31 -0700

    Bumped musicbot version

--- containers/musicbot/jmusicbot.nix
+++ containers/musicbot/jmusicbot.nix
@@ -5,11 +5,11 @@
 }:
 stdenv.mkDerivation rec {
   name = "JMusicBot";
-  version = "0.4.1";
+  version = "0.4.2";
   src = fetchurl {
     url = "https://github.com/jagrosh/MusicBot/releases/download/${version}/${name}-${version}.jar";
     # sha256 = "sha256-JSVrzyCqAp3V5OZ+KJczhWGolPkdaHsPmiqfmhapQMs"; # 0.4.0 is broken
-    sha256 = "sha256-+0814w4zKNr2TxZ9CS8FxeuTLa71jM+DhrfFvLMFlw0=";
+    sha256 = "sha256-Jg6/ju3ADBd7fc3njRzoEDVjIL4SzAzlTc02I4Q9hz4=";
   };
 
   buildInputs = [jdk11_headless];
