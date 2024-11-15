c733c3fc41c08ed8a8c62e7ae0f7f09ef1ed04de
kylosus <jokersus.cava@gmail.com>
2024-11-15 10:58:52 -0600

    Attempts made at making nixos bootable with windows 11

--- modules/nixos/base/boot.nix
+++ modules/nixos/base/boot.nix
@@ -16,9 +16,13 @@
   boot.initrd.kernelModules = lib.mkDefault ["xhci_pci" "usbhid" "usb_storage"];
   boot.extraModulePackages = lib.mkDefault [];
 
-  boot.loader.systemd-boot.enable = builtins.hasAttr "/boot" config.fileSystems;
+  boot.loader = lib.mkIf (builtins.hasAttr "bootDisk" params.fs) (lib.mkDefault {
+    efi.canTouchEfiVariables = true;
+    efi.efiSysMountPoint = "/boot";
+    systemd-boot.enable = true;
+  });
 
-  #boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
+  # boot.loader.systemd-boot.enable = builtins.hasAttr "/boot" config.fileSystems;
 
   boot.initrd.luks.devices = lib.mkIf (builtins.hasAttr "luksDisk" params.fs) (lib.mkDefault {
     root = {
