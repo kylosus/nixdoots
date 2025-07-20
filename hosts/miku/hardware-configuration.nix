56c4a8b9ac67422c7cfe93524b31a8c56e6876d6
kylosus <jokersus.cava@gmail.com>
2025-07-19 21:36:28 -0500

    Added mkBtrfs for easy hardware-config

--- hosts/miku/hardware-configuration.nix
+++ hosts/miku/hardware-configuration.nix
@@ -1,75 +1,19 @@
-{
-  config,
-  lib,
-  pkgs,
-  modulesPath,
-  ...
-}: let
+{functions, ...}: let
   device = "/dev/disk/by-uuid/f43d05ae-88a0-4bb4-b656-7dc403266441";
-  btrfsOptions = ["relatime" "compress-force=zstd"];
 in {
-  fileSystems."/" = {
+  fileSystems = functions.mkBtrfs {
     inherit device;
-    fsType = "btrfs";
-    options = ["subvol=@/.snapshots/1/snapshot"] ++ btrfsOptions;
-  };
-
-  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/dd127f93-cc9f-44de-9400-145b2fac9fae";
-
-  # Needed when /tmp on btrfs
-  boot.tmp.cleanOnBoot = true;
-
-  fileSystems."/.snapshots" = {
-    inherit device;
-    fsType = "btrfs";
-    options = ["subvol=@/.snapshots"] ++ btrfsOptions;
-  };
-
-  fileSystems."/nix" = {
-    inherit device;
-    fsType = "btrfs";
-    options = ["subvol=@/nix"] ++ btrfsOptions;
-  };
-
-  fileSystems."/home" = {
-    inherit device;
-    fsType = "btrfs";
-    options = ["subvol=@/home"] ++ btrfsOptions;
-  };
-
-  fileSystems."/opt" = {
-    inherit device;
-    fsType = "btrfs";
-    options = ["subvol=@/opt"] ++ btrfsOptions;
-  };
-
-  fileSystems."/root" = {
-    inherit device;
-    fsType = "btrfs";
-    options = ["subvol=@/root"] ++ btrfsOptions;
-  };
-
-  fileSystems."/srv" = {
-    inherit device;
-    fsType = "btrfs";
-    options = ["subvol=@/srv"] ++ btrfsOptions;
-  };
-
-  fileSystems."/tmp" = {
-    inherit device;
-    fsType = "btrfs";
-    options = ["subvol=@/tmp"] ++ btrfsOptions;
-  };
-
-  fileSystems."/usr/local" = {
-    inherit device;
-    fsType = "btrfs";
-    options = ["subvol=@/usr/local"] ++ btrfsOptions;
-  };
-
-  fileSystems."/var" = {
-    inherit device;
-    fsType = "btrfs";
-    options = ["subvol=@/var"] ++ btrfsOptions;
+    rootSubvol = ".snapshots/1/snapshot";
+    subvols = [
+      ".snapshots"
+      "nix"
+      "home"
+      "opt"
+      "root"
+      "srv"
+      "tmp"
+      "usr/local"
+      "var"
+    ];
   };
 }
--- lib/functions.nix
+++ lib/functions.nix
@@ -12,4 +12,30 @@
     overlays.unstable-packages
     overlays.stable-packages
   ];
+
+  mkBtrfs = {
+    device,
+    rootSubvol ? ".snapshots/1/snapshot",
+    subvols,
+    btrfsOptions ? ["relatime" "compress-force=zstd"],
+  }: let
+    mkEntry = subvol: {
+      name = "/${subvol}";
+      value = {
+        inherit device;
+        fsType = "btrfs";
+        options = ["subvol=@/${subvol}"] ++ btrfsOptions;
+      };
+    };
+
+    rootEntry = {
+      name = "/";
+      value = {
+        inherit device;
+        fsType = "btrfs";
+        options = ["subvol=@/${rootSubvol}"] ++ btrfsOptions;
+      };
+    };
+  in
+    builtins.listToAttrs ([rootEntry] ++ map mkEntry subvols);
 }
