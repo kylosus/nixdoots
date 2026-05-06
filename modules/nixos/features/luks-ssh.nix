9dd7443792a0bafd821f56b129d359d630cef11d
kylosus <jokersus.cava@gmail.com>
2026-05-06 14:40:36 +0300

    Removed cryptsetup shell  for nix 26.05

--- modules/nixos/features/luks-ssh.nix
+++ modules/nixos/features/luks-ssh.nix
@@ -14,7 +14,6 @@
         authorizedKeyFiles = [files.ssh-authorized];
         # ssh-keygen -t rsa -N "" -f /etc/secrets/initrd/ssh_host_rsa_key
         hostKeys = ["/etc/secrets/initrd/ssh_host_rsa_key"];
-        shell = "/bin/cryptsetup-askpass";
       };
     };
   };
