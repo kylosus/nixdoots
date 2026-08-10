0ed6c998c8341a8d772d73241fde51e268c94471
kylosus <jokersus.cava@gmail.com>
2026-08-10 12:53:41 +0400

    Updated dependencies +
    Removed eza war

--- overlays/default.nix
+++ overlays/default.nix
@@ -45,9 +45,6 @@ in {
     #   };
     # };
 
-    # TODO: bug fixes
-    eza = final.stable.eza;
-
     # Slow compilation fixes. Check on next update
     # gtk4 = final.stable.gtk4;
     # nvidia-vaapi-driver = final.stable.nvidia-vaapi-driver;
